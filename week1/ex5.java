eab84e0fbcb9302342497a5e64b00e26c8e7a7f3
Marek <marek.tammevali@voco.ee>
2022-09-02 15:17:35 +0300

    ex5

---
+++ week1/ex5.java
@@ -0,0 +1,11 @@
+package week1;
+
+public class ex5 {
+    public static void main(String[] args) {
+        int days= 365;
+        int hours= 24;
+        int minutes= 60;
+        int seconds = 60;
+
+    }
+}
