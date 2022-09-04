2e1a2ca6f2898b32ea4a407723b800158484136e
Marek <marek.tammevali@voco.ee>
2022-09-05 00:10:50 +0300

    ex21 week1

---
+++ week1/ex21.java
@@ -0,0 +1,16 @@
+package week1;
+import java.util.Scanner;
+public class ex21 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Type a year: ");
+        int year = reader.nextInt();
+
+        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+            System.out.print("The year is a leap year.");
+        } else {
+            System.out.print("The year is not a leap year.");
+        }
+
+    }
+}
