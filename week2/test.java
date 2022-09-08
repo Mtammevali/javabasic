1740d2a62f7d1e4a7f2338fbf804b482948b54db
Marek <marek.tammevali@voco.ee>
2022-09-08 12:27:52 +0300

    ex36 week2 sum +count

---
+++ week2/test.java
@@ -0,0 +1,25 @@
+package week2;
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+    System.out.println("Type your age: ");
+
+    int age = Integer.parseInt(reader.nextLine());
+
+while (age < 5 || age > 85) {  // age less than 5 OR greater than 85
+        System.out.println("You are lying!");
+        if (age < 5) {
+            System.out.println("You are so young that you cannot know how to write!");
+        } else if (age > 85) {
+            System.out.println("You are so old that you cannot know how to use a computer!");
+        }
+
+        System.out.println("Type your age again: ");
+        age = Integer.parseInt(reader.nextLine());
+    }
+
+System.out.println("Your age is " + age);
+}
+}
