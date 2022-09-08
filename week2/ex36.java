33ab2a7139e7029cbdfce3891f80160bae4eb4a8
Marek <marek.tammevali@voco.ee>
2022-09-08 12:39:00 +0300

    ex36 week2 even or odd number

--- week2/ex36.java
+++ week2/ex36.java
@@ -9,6 +9,8 @@ public class ex36 {
         int number;
         int sum = 0;
         int count = 0;
+        int even = 0;
+        int odd = 0;
         while (true) {
 
 
@@ -19,7 +21,12 @@ public class ex36 {
             }
             sum += number;//sum= sum+number
             count++;
-
+            if(number % 2 == 0){
+                even++;
+            }
+            else {
+                odd++;
+            }
         }
 
         System.out.println("Thank you see you later");
@@ -27,6 +34,8 @@ public class ex36 {
         System.out.println("how many numbers?:" + count);
         double average = (double)sum / count;
         System.out.println("average: "+ average);
+        System.out.println("even numbers: "+ even);
+        System.out.println("odd numbers: "+ odd);
     }
 }
 
