722831525fb272fd98e20b794726bde13a27aa1b
Marek <marek.tammevali@voco.ee>
2022-09-09 09:18:21 +0300

    ex72 week4 matts to my acc

--- week4/ex72.java
+++ week4/ex72.java
@@ -3,22 +3,16 @@ package week4;
 public class ex72 {
     public static void main(String[] args) {
 
+        Account mattAccount = new Account("Matt's account",1000.00);
+        Account myAccount = new Account("My account ",0);
 
-    Account bartosAccount = new Account("Barto's account",100.00);
-    Account bartosSwissAccount = new Account("Barto's account in Switzerland",1000000.00);
+        System.out.println(mattAccount);
+        System.out.println(myAccount);
 
-System.out.println("Initial state");
-System.out.println(bartosAccount);
-System.out.println(bartosSwissAccount);
+        mattAccount.withdrawal(100);
+        System.out.println(mattAccount);
 
-bartosAccount.withdrawal(20);
-System.out.println("Barto's account balance is now: "+bartosAccount.balance());
-bartosSwissAccount.deposit(200);
-System.out.println("Barto's Swiss account balance is now: "+bartosSwissAccount.balance());
-
-System.out.println("Final state");
-System.out.println(bartosAccount);
-System.out.println(bartosSwissAccount);
-
+        myAccount.deposit(100);
+        System.out.println(myAccount);
 }
 }
