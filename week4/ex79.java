7544d2c696e9a9bb53e35c4e20e2429ef7cafa4e
Marek <marek.tammevali@voco.ee>
2022-09-09 11:05:52 +0300

    ex73 week4 amount , sum average

---
+++ week4/ex79.java
@@ -0,0 +1,15 @@
+package week4;
+
+public class ex79 {
+    public static void main(String[] args) {
+        NumberStatistics stats = new NumberStatistics();
+        stats.addNumber(0);
+        stats.addNumber(5);
+        stats.addNumber(1);
+        stats.addNumber(2);
+        stats.addNumber(2);
+        System.out.println("Amount: " + stats.amountOfNumbers());
+        System.out.println("sum: " + stats.sum());
+        System.out.println("average: " + stats.average());
+    }
+}
