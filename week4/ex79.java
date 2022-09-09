210ae42d2636e573b5041735b63c70868b996e08
Marek <marek.tammevali@voco.ee>
2022-09-09 11:50:57 +0300

    ex79 week4 sum, even and odd

--- week4/ex79.java
+++ week4/ex79.java
@@ -8,7 +8,7 @@ public class ex79 {
         System.out.println("Type your number: ");
         Scanner reader = new Scanner(System.in);
         int number;
-        int sum = 0;
+        //int sum = 0;
         int count = 0;
         int even = 0;
         int odd = 0;
@@ -20,7 +20,8 @@ public class ex79 {
             if (number == -1) {
                 break;
             }
-            sum += number;//sum= sum+number
+            stats.addNumber(number);
+            //sum += number;//sum= sum+number
             count++;
             if(number % 2 == 0){
                 even++;
@@ -35,8 +36,8 @@ public class ex79 {
 
 
         //System.out.println("Amount: " + stats.amountOfNumbers());
-        //System.out.println("sum: " + stats.sum());
-        System.out.println("The sum is " + sum);
+        System.out.println("sum: " + stats.sum());
+        //System.out.println("The sum is " + sum);
         //System.out.println("average: " + stats.average());
         System.out.println("even numbers: "+ even);
         System.out.println("odd numbers: "+ odd);
