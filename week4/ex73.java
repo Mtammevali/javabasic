d786a9c23b7972b9d232e5f77b59f59627b0c2c5
Marek <marek.tammevali@voco.ee>
2022-09-09 09:48:54 +0300

    ex73 week4 Product

---
+++ week4/ex73.java
@@ -0,0 +1,12 @@
+package week4;
+
+public class ex73 {
+    public static void main(String[] args) {
+            Product banana = new Product("Banana", 1, 10);
+
+
+            banana.printProduct();
+
+
+    }
+}
