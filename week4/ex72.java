3abd4fe5286dd7a690c03cb3efd12a3e46edc24e
Marek <marek.tammevali@voco.ee>
2022-09-09 09:24:55 +0300

    ex72 week4 matts to my acc and tranfere between accounts

--- week4/ex72.java
+++ week4/ex72.java
@@ -14,5 +14,28 @@ public class ex72 {
 
         myAccount.deposit(100);
         System.out.println(myAccount);
+
+        Account a = new Account("A", 1000);
+        Account b = new Account("B", 1);
+        Account c = new Account("C", 0);
+
+
+
+        transfere(a, b, 50 );
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        transfere(b, c, 25 );
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
 }
+
+    public static void transfere(Account from, Account to, double amount ) {
+        from.withdrawal(amount);
+        to.deposit(amount);
+    }
 }
