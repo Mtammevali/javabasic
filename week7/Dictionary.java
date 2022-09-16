5734fc30a97e0358aeb1b3d1cb66251582f25e61
Marek <marek.tammevali@voco.ee>
2022-09-16 12:51:30 +0300

    ex7.1 Dictionary

---
+++ week7/Dictionary.java
@@ -0,0 +1,23 @@
+package week7;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class Dictionary {
+    private HashMap<String, String> dictionary;
+
+    public Dictionary() {
+        this.dictionary = new HashMap<String, String>();
+
+    }
+
+    public void add(String word, String translation) {
+        dictionary.put(word, translation);
+    }
+
+    public String translate(String word) {
+        return dictionary.get(word);
+    }
+
+}
+
