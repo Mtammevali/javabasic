760c5eaf181514c22e28f9f8a6676b9eebe1952c
Marek <marek.tammevali@voco.ee>
2022-09-23 09:59:48 +0300

    Ex20.1: Constant Sensor

---
+++ week9/measurments/Sensor.java
@@ -0,0 +1,11 @@
+package week9.measurments;
+
+public interface Sensor {
+
+        boolean isOn();  // returns true if the sensor is on
+        void on();       // switches the sensor on
+        void off() throws IllegalStateException; // switches the sensor off
+        int measure();   // returns the sensor reading if the sensor is on
+        // if the sensor is off, it throws an IllegalStateException
+
+}
