6a1005118c8d19e8d4336bf4930cfbd62acc3c42
Marek <marek.tammevali@voco.ee>
2022-09-23 11:16:24 +0300

    Ex20.3: AverageSensor

--- week9/ex20.java
+++ week9/ex20.java
@@ -1,15 +1,26 @@
 package week9;
 
+import week9.measurments.AverageSensor;
 import week9.measurments.ConstantSensor;
 import week9.measurments.Sensor;
 import week9.measurments.Thermometer;
+
 
 public class ex20 {
     public static void main(String[] args) {
         Sensor kumpula = new Thermometer();
         kumpula.on();
         System.out.println("the temperature in Kumpula is "+kumpula.measure() + " degrees");
-        kumpula.off();
-        System.out.println("the temperature in Kumpula is "+kumpula.measure() + " degrees");
+
+        Sensor kaisaniemi = new Thermometer();
+        Sensor helsinkiVantaa = new Thermometer();
+
+        AverageSensor helsinkiArea = new AverageSensor();
+        helsinkiArea.addSensor(kumpula);
+        helsinkiArea.addSensor(kaisaniemi);
+        helsinkiArea.addSensor(helsinkiVantaa);
+
+        helsinkiArea.on();
+        System.out.println("the temperature in Helsinki area is "+helsinkiArea.measure() + " degrees");
     }
 }
