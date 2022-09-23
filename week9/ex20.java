be74a95fcafecb50018c4698894f376865b9baf1
Marek <marek.tammevali@voco.ee>
2022-09-23 10:44:22 +0300

    Ex20.2: Thermometer

--- week9/ex20.java
+++ week9/ex20.java
@@ -1,17 +1,15 @@
 package week9;
 
 import week9.measurments.ConstantSensor;
+import week9.measurments.Sensor;
+import week9.measurments.Thermometer;
 
 public class ex20 {
     public static void main(String[] args) {
-        ConstantSensor ten = new ConstantSensor(10);
-        ConstantSensor minusFive = new ConstantSensor(-5);
-
-        System.out.println(ten.measure());
-        System.out.println(minusFive.measure());
-
-        System.out.println(ten.isOn());
-        ten.off();
-        System.out.println(ten.isOn());
+        Sensor kumpula = new Thermometer();
+        kumpula.on();
+        System.out.println("the temperature in Kumpula is "+kumpula.measure() + " degrees");
+        kumpula.off();
+        System.out.println("the temperature in Kumpula is "+kumpula.measure() + " degrees");
     }
 }
