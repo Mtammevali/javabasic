ad83283e880886b3b7473f9bdfdac4360ce2f5cb
Marek <marek.tammevali@voco.ee>
2022-09-30 09:04:39 +0300

    week 6- ex18,1 - Things and Items

---
+++ week9/ex18.java
@@ -0,0 +1,11 @@
+package week9;
+
+import week9.mooving.domain.Item;
+import week9.mooving.domain.Thing;
+
+public class ex18 {
+    public static void main(String[] args) {
+        Thing item = new Item("toothbrush", 2);
+        System.out.println(item);
+    }
+}
